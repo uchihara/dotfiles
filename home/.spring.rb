aa29b18d4d19a652590af4db51e2b7932c08bea9
Akira Uchihara <uchihara@tonchidot.com>
2015-01-31 15:24:59 +0900

    applied macosx configurations

--- home/.spring.rb
+++ home/.spring.rb
@@ -14,5 +14,19 @@ module Spring
       end
     end
     Spring.register_command "unicorn", Unicorn.new
+    class UnicornRails
+      def env(args)
+        "development"
+      end
+
+      def exec_name
+        "unicorn_rails"
+      end
+
+      def description
+        "start unicorn_rails server."
+      end
+    end
+    Spring.register_command "unicorn_rails", UnicornRails.new
   end
 end
